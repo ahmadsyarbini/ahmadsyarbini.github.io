75c9b09aab1411b61accd16dc278fc50b895b9b0
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-01-15 17:31:08 +0800

    update

--- git-push-changes.cmd
+++ git-push-changes.cmd
@@ -1,6 +1,7 @@
 @echo off
 rem this is my automated git push. dont worry about it
 cd /d E:\ahmadsyarbini.github.io
+git pull --rebase origin main
 git add .
 git commit -m "update"
 git push origin main
