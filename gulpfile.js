c4a142476cc99c5c0ef780a41a034a0021ae5bd4
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-04-07 16:32:01 +0800

    update

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const { watch, series } = require('gulp');
+const browserSync = require('browser-sync').create();
+
+// Function to start BrowserSync proxy
+function serve(cb) {
+    browserSync.init({
+        proxy: "http://127.0.0.1:8000", // Python server
+        port: 3001
+    });
+    cb();
+}
+
+// Watch files and reload
+function watchFiles() {
+    watch(['**/*.html', '**/*.css', '**/*.js', '!node_modules/**'])
+        .on('change', browserSync.reload);
+}
+
+// Default task: start server and watch files
+exports.default = series(serve, watchFiles);
--- py-start-localhost.cmd
+++ py-start-localhost.cmd
@@ -1,4 +1,14 @@
 @echo off
 rem this is the script for localhost using python
 rem cd /d E:\ahmadsyarbini.github.io
-python -m http.server 8000
+
+rem python -m http.server 8000 || npx gulp
+
+REM Start Python server in background
+start "" cmd /k "python -m http.server 8000"
+
+REM Wait a second for server to start (optional)
+timeout /t 1 /nobreak > nul
+
+REM Start Gulp watch
+npx gulp
