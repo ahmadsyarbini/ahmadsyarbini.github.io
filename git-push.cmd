790f69f56f90d8bd8562c23ca5c1643dd7cafd18
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-04-07 15:50:23 +0800

    update

--- git-push.cmd
+++ git-push.cmd
@@ -1,6 +1,6 @@
 @echo off
 rem this is just the workflow of push. dont worry.
-cd /d E:\ahmadsyarbini.github.io
+rem cd /d E:\ahmadsyarbini.github.io
 git add .
 git commit -m "update"
 git push origin main
--- py-start-localhost.cmd
+++ py-start-localhost.cmd
@@ -1,4 +1,4 @@
 @echo off
 rem this is the script for localhost using python
-cd /d E:\ahmadsyarbini.github.io
+rem cd /d E:\ahmadsyarbini.github.io
 python -m http.server 8000
