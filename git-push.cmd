53d4158ebf1fbec0f7349a7ab81627532f8405b4
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-04-09 18:39:14 +0800

    update

--- git-push.cmd
+++ git-push.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem this is just the workflow of push. dont worry.
 rem cd /d E:\ahmadsyarbini.github.io
-git add .
+git add -A
 git commit -m "update"
-git push origin main
+git push origin main --force
 timeout /t 4 /nobreak >nul
